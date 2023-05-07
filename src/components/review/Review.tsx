036343ca9996be4d9100932f27237f894398f863
YuOshima <bigislandyu31@gmail.com>
2023-05-07 23:16:54 +0900

    Review-0507-commit

--- src/components/review/Review.tsx
+++ src/components/review/Review.tsx
@@ -1,15 +1,38 @@
 import classes from "./Review.module.scss";
-import { storage } from "../../firebase";
-import { ref, uploadBytesResumable } from "firebase/storage";
+import { db, storage } from "../../firebase";
+import {
+  StorageReference,
+  getDownloadURL,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
 import { useState } from "react";
+import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { useAppSelector } from "../../app/hooks";
 
 const Review = () => {
   const [loading, setLoading] = useState(false);
   const [isUploaded, setUploaded] = useState(false);
+  const [title, setTitle] = useState("");
+
+  const user = useAppSelector((state) => state.user);
+
+  const addReview = async () => {
+    await addDoc(collection(db, "reviews"), {
+      titleName: title,
+      uid: { user },
+      photoURL: "",
+      createdAt: serverTimestamp(),
+    });
+  };
 
   const OnFileUploadToFirebase = (e: any) => {
     const file = e.target.files[0];
-    const storageRef = ref(storage, "image/" + file.name);
+    const ext = file.name.split(".").pop();
+    const fileName = `${Date.now()}.${ext}`;
+    const filePath = `images/${fileName}`;
+    const storageRef = ref(storage, filePath);
     const uploadImage = uploadBytesResumable(storageRef, file);
     uploadImage.on(
       "state_changed",
@@ -22,6 +45,11 @@ const Review = () => {
       () => {
         setLoading(false);
         setUploaded(true);
+        const newImage: {
+          id: string;
+          path: (ref: StorageReference) => Promise<string>;
+        } = { id: fileName, path: getDownloadURL };
+        console.log(newImage.path);
       }
     );
   };
@@ -40,40 +68,55 @@ const Review = () => {
       </div>
       <div className={classes.title}>
         <p className={classes.titleText}>タイトル</p>
-        <textarea className={classes.titleInput} name="title"></textarea>
+        <form>
+          <textarea
+            onChange={(e) => setTitle(e.target.value)}
+            className={classes.titleInput}
+            name="title"
+          ></textarea>
+        </form>
       </div>
       <div className={classes.file}>
         <p className={classes.fileText}>画像</p>
-        <input
-          className={classes.fileInput}
-          multiple
-          type="file"
-          name="imageURL"
-          accept=".png, .jpeg, .jpg"
-          onChange={OnFileUploadToFirebase}
-        ></input>
-        {loading ? (
-          <p className={classes.fileBoolean}>アップロード中...</p>
-        ) : (
-          <>
-            {isUploaded ? (
-              <p className={classes.fileBoolean}>
-                ファイルアップロードが完了しました！
-              </p>
-            ) : (
-              <p className={classes.fileBoolean}>
-                ファイルが選択されていません
-              </p>
-            )}
-          </>
-        )}
+        <label>
+          <input
+            className={classes.fileInput}
+            multiple
+            type="file"
+            id="image"
+            name="imageURL"
+            accept=".png, .jpeg, .jpg"
+            onChange={OnFileUploadToFirebase}
+          ></input>
+          {loading ? (
+            <p className={classes.fileUploading}>アップロード中...</p>
+          ) : (
+            <>
+              {isUploaded ? (
+                <>
+                  {/* <img alt="プレビュー画像" src={newImage.path}></img> */}
+                  <p className={classes.fileTrue}>
+                    ファイルアップロードが完了しました！
+                  </p>
+                </>
+              ) : (
+                <div className={classes.materialIconContainer}>
+                  <AddPhotoAlternateIcon className={classes.materialIcon} />
+                  <p className={classes.fileFalse}>ファイルを選択</p>
+                </div>
+              )}
+            </>
+          )}
+        </label>
       </div>
       <div className={classes.main}>
         <p className={classes.mainText}>本文</p>
         <textarea className={classes.mainInput} name="main"></textarea>
       </div>
       <div className={classes.buttonContainer}>
-        <button className={classes.button}>送信</button>
+        <button onClick={addReview} className={classes.button}>
+          送信
+        </button>
       </div>
     </div>
   );
